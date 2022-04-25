769db56ba2421805fa7491521c1b1db01a79e773
kareem96 <ak339444@gmail.com>
2022-04-25 16:21:38 +0700

    update 25/04/2022

--- Latihan Service/MyService/app/src/main/java/com/kareemdev/myservice/MainActivity.kt
+++ Latihan Service/MyService/app/src/main/java/com/kareemdev/myservice/MainActivity.kt
@@ -1,11 +1,55 @@
 package com.kareemdev.myservice
 
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private var mServiceBound = false
+    private lateinit var mBoundService: MyBoundService
+
+    private val mServiceConnection = object : ServiceConnection{
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            val myBinder = service as MyBoundService.MyBinder
+            mBoundService = myBinder.getService
+            mServiceBound = true
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            mServiceBound = false
+        }
+
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnStartService = findViewById<Button>(R.id.btn_start_service)
+        btnStartService.setOnClickListener {
+            val mStartServiceIntent = Intent(this, MyService::class.java)
+            startService(mStartServiceIntent)
+        }
+
+        val btnStartBoundService = findViewById<Button>(R.id.btn_start_bound_service)
+        btnStartBoundService.setOnClickListener {
+            val mBoundServiceIntent = Intent(this, MyBoundService::class.java)
+            bindService(mBoundServiceIntent, mServiceConnection, BIND_AUTO_CREATE)
+        }
+
+        val btnStopBoundService = findViewById<Button>(R.id.btn_stop_bound_service)
+        btnStopBoundService.setOnClickListener {
+            unbindService(mServiceConnection)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if(mServiceBound){
+            unbindService(mServiceConnection)
+        }
     }
 }
