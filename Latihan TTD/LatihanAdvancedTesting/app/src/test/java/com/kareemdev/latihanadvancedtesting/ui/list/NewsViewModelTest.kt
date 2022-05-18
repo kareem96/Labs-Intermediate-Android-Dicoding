0120d847e839099c994cc8d9705b756f475bc9eb
kareem96 <ak339444@gmail.com>
2022-05-18 17:01:18 +0700

    update 18/05/2022

--- Latihan TTD/LatihanAdvancedTesting/app/src/test/java/com/kareemdev/latihanadvancedtesting/ui/list/NewsViewModelTest.kt
+++ Latihan TTD/LatihanAdvancedTesting/app/src/test/java/com/kareemdev/latihanadvancedtesting/ui/list/NewsViewModelTest.kt
@@ -1,5 +1,44 @@
 package com.kareemdev.latihanadvancedtesting.ui.list
 
-import org.junit.Assert.*
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import com.kareemdev.latihanadvancedtesting.data.NewsRepository
+import com.kareemdev.latihanadvancedtesting.data.local.entity.NewsEntity
+import com.kareemdev.latihanadvancedtesting.utils.DataDummy
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import com.kareemdev.latihanadvancedtesting.data.Result
+import org.junit.Rule
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.junit.MockitoJUnitRunner
 
-class NewsViewModelTest
+@RunWith(MockitoJUnitRunner::class)
+class NewsViewModelTest{
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+    @Mock
+    private lateinit var newsRepository: NewsRepository
+    private lateinit var viewModel: NewsViewModel
+    private val dummyNews =  DataDummy.generateDummyNewsEntity()
+    @Before
+    fun setUp(){
+        viewModel = NewsViewModel(newsRepository)
+    }
+    @Test
+    fun `when Get headlinesNews Should Not null and Return Success`(){
+        val observer = Observer<Result<List<NewsEntity>>>{}
+        try {
+            val expectedNews = MutableLiveData<Result<List<NewsEntity>>>()
+            expectedNews.value = Result.Success(dummyNews)
+            `when`(viewModel.getHeadlineNews()).thenReturn(expectedNews)
+            val actualNews = viewModel.getHeadlineNews()
+            Assert.assertNotNull(actualNews)
+        }finally {
+            viewModel.getHeadlineNews().removeObserver(observer)
+        }
+    }
+}
